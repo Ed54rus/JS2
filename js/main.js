ff2d0ef0dbb6d3469d811751a857b1b4c14d0810
Eduard Malyshenko <ed54rus@mail.ru>
2023-01-11 19:13:48 +0700

    преределал рендеринг товаров Ðклассами

--- js/main.js
+++ js/main.js
@@ -1,23 +1,50 @@
-const products = [
-    {id: 14, title: 'Ноутбук', price: 72000},
-    {id: 256, title: 'Монитор', price: 34000},
-    {id: 66, title: 'Наушники', price: 6000},
-    {id: 789, title: 'Веб камера', price: 2000},
-    {id: 456, title: 'Мышь', price: 5000}
-];
-
-const renderProduct = (product, img='https://via.placeholder.com/200x150') => {
-    return `<div class="product-item">
-                <img src="${img}" alt="Product picture" width="200" height="150">
-                <div class="product-item__desc">
-                    <h3>${product.title}</h3>
-                    <p>${product.price} руб.</p>
-                    <button class="buy-btn">Купить</button>
-                </div>
-            </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item)).join('');
-    document.querySelector('.products').insertAdjacentHTML('beforeend', productsList);
+class ProductsList {
+	constructor(container = '.products') {
+		this.container = container;
+		this.goods = [];
+		this.allProducts = [];
+		this._fetchProducts();
+	}
+	
+	_fetchProducts() {
+		this.goods = [
+			{id: 14, title: 'Ноутбук', price: 72000},
+			{id: 256, title: 'Монитор', price: 34000},
+			{id: 66, title: 'Наушники', price: 6000},
+			{id: 789, title: 'Веб камера', price: 2000},
+			{id: 456, title: 'Мышь', price: 5000},
+		]
+	}
+	
+	render() {
+		const block = document.querySelector(this.container);
+		for (let product of this.goods) {
+			const prodObj = new ProductItem(product);
+			this.allProducts.push(prodObj);
+			block.insertAdjacentHTML('beforeend', prodObj.render());
+		}
+	}
 }
-renderPage(products);
+
+class ProductItem {
+	constructor(product, img = 'https://via.placeholder.com/200x150') {
+		this.title = product.title;
+		this.price = product.price;
+		this.id = product.id;
+		this.img = img;
+	}
+	
+	render() {
+		return `<div class="product-item" data-id="${this.id}">
+                    <img src="${this.img}" alt="Product picture" width="200" height="150">
+                    <div class="product-item__desc">
+                       <h3>${this.title}</h3>
+                       <p>${this.price} руб.</p>
+                       <button class="buy-btn">Купить</button>
+                    </div>
+                 </div>`
+	}
+}
+
+let list = new ProductsList();
+list.render();
